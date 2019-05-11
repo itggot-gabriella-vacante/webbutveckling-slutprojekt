56d722dccedbbe79320ad893121e86634185a607
itggot-gabriella-vacante <gabriella.vacante@elev.ga.ntig.se>
2019-05-11 15:00:16 +0200

    Menu
    Created main.js and linked it to index.html, copied and added js code from previous project, styled the menu with classes such as fade-layer and menu, created the class manuitems and changed the font and font-size

---
+++ site/js/main.js
@@ -0,0 +1,8 @@
+/*Menu*/
+function showMenu(event){
+    let menu = document.querySelector(".menu")
+    menu.classList.toggle("show")
+  
+    let fadeLayer = document.querySelector(".fade-layer")
+    fadeLayer.classList.toggle("visible")
+}
